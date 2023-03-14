157fdd28966b226ae151d9ea88d701b11654b4fd
KmBase <1462011227@qq.com>
2023-03-15 00:04:35 +0800

    update

--- run.py
+++ run.py
@@ -1,18 +1,24 @@
 import sys
 import requests
+import flask,json
+
 from flask import Flask, request
 
 app = Flask(__name__)
-# app.secret_key = 'security-guard'
-{'proxy_url':'https://kmbase.eu.org','api_key':'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','question':'hello'}
-@app.route('/api/count', methods=['POST'])
-def user_api():
-    proxy_url = request.json['proxy_url']
-    api_key = request.json['api_key']
-    question = request.json['question']  # 获取表单数据
+app.secret_key = 'security-guard'
+
+@app.route('/chat',methods=['post'])
+def chat():
+    data = request.data.decode('utf-8')
+    data = json.loads(data)
+    question = data['question']  # 获取表单数据
+    proxy_url = data['proxy_url']
+    api_key = data['api_key']
+    print(question)
     # The text prompt you want to generate a response
     # The URL for OpenAI's API
     url = f"{proxy_url}/v1/chat/completions"
+    print(url)
     # The headers for the API request
     headers = {"Content-Type": "application/json","Authorization": f"Bearer {api_key}"}
     data = {"model":"gpt-3.5-turbo","messages": [{"role":"user","content":question}],"max_tokens":800,"temperature":0.5,"frequency_penalty":0,"presence_penalty":0}
@@ -25,10 +31,14 @@ def user_api():
         usage = response.json()['usage']
         print(generated_text)
         print(usage)
+        return generated_text
     else:
         # Handle the error
+        generated_text = None
+        usage = None
         print(f"Request failed with status code {response.status_code}")
-    return generated_text
+        return None
+
 
 # 启动Flask Web服务
 if __name__ == '__main__':
--- serve.py
+++
@@ -1,4 +0,0 @@
-import request
-
-header = {'proxy_url':'https://kmbase.eu.org','api_key':'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','question':'hello'}
-request.post(header)
